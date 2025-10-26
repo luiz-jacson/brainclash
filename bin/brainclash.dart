f2924c9a48a75dfd65bdad10741b0cf014665344
luiz982 <juninholjl@hotmail.com>
2025-10-25 22:11:36 -0300

    gerando perguntas com gemini model 2.5 flash

--- bin/brainclash.dart
+++ bin/brainclash.dart
@@ -1,9 +1,10 @@
+import 'package:brainclash/models/categoria.dart';
 import 'package:brainclash/screens/menu.dart';
 import 'package:brainclash/service/categoria_service.dart';
+import 'package:brainclash/service/pergunta_service.dart';
 
 void main() async{
   //menu();
- CategoriaService categoriaService = CategoriaService();
- categoriaService.gerarCategoriaGemini();
-  
+  PerguntaService perguntaService = PerguntaService();
+  perguntaService.gerarPerguntaGemini(Categoria("Ciências", 1)); 
 }
